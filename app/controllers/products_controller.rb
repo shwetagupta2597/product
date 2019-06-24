edeee042bf2c790919dfe2f1bc80ebc215e4577b
SHWETA <shweta.gupta@terralogic.com>
2019-06-24 15:56:17 +0530

    Sign in before add to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   # GET /products
   # GET /products.json
